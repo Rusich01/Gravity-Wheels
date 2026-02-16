fac1e2e23159f440b156c6ae612e5b6498f8ea8f
Ruslan Hrytsai <103769765+Rusich01@users.noreply.github.com>
2026-02-16 01:08:13 +0100

    start project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import MainPages from "./pages/main-pages";
+
+const App = () => {
+  return (
+    <div>
+      <MainPages />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/main-pages.tsx
@@ -0,0 +1,11 @@
+import HeaderRoot from "../components/header/header-root";
+
+const MainPages = () => {
+  return (
+    <div>
+      <HeaderRoot />
+    </div>
+  );
+};
+
+export default MainPages;
